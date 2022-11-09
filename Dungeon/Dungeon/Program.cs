118ede40ea3313fd51fce99e52a1d7aaffca7cb8
DanielAmbro <DanielAmbro1@outlook.com>
2022-11-08 20:34:08 -0600

    Dungeon homework

---
+++ Dungeon/Dungeon/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Dungeon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            #region Introduction
+            //Print a welcome.
+            #endregion
+
+            #region Create Player
+            //TODO: Create an instance of the Player class.
+            #endregion
+
+            #region Gameplay Loop
+
+            #region Create Room & Monster
+
+            //TODO: Print a random room description.
+            //TODO: Create an instance of a Monster class at random.
+
+            #endregion
+
+            #region Menu Loop
+
+            /*
+                TODO: Create menu with options:
+                1) Attack
+                2) Run Away
+                3) Character Info
+                4) Monster Info
+                5) Exit
+             */
+
+            #endregion
+
+            #endregion
+        }
+    }
+}
