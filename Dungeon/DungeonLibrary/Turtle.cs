aa8b19080bf5e9fbd9b20d4f233e4b07e774d10e
DanielAmbro <DanielAmbro1@outlook.com>
2022-11-21 20:30:11 -0600

    Making more Monsters

---
+++ Dungeon/DungeonLibrary/Turtle.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Turtle : Monster
+    {
+        //Inherit from Monster ^
+
+        //Add unique properties 
+        public int BonusBlock { get; set; }
+        public int HidePercent { get; set; }
+
+        //Add those unique properties to the parameters of the constructor:
+        public Turtle(string name, string description, int life, int maxLife, int hitChance, int block,
+            int minDamage, int maxDamage, int bonusBlock, int hidePercent) : base(name, description, hitChance, 
+                block, life, maxLife, minDamage, maxDamage)
+        {
+            //Assign the unique properties
+            BonusBlock = bonusBlock;
+            HidePercent = hidePercent;
+        }
+
+        //EXAMPLE: Default values with an empty ctor
+        public Turtle() 
+        {
+            Name = "Michaelangelo";
+            Description = "Turtle Power!";
+            MaxLife = 70;
+            Life = 70;
+            HitChance = 60;
+            Block = 15;
+            MinDamage = 20;
+            MaxDamage = 30;
+            BonusBlock = 20;
+            HidePercent = 25;
+        }
+
+        //EXAMPLE: Override the ToString() using the unique property
+        public override string ToString()
+        {
+            return base.ToString() + string.Format("\n{0}% chance it will hide, granting {1} bonus block.",
+                 HidePercent, BonusBlock);
+        }
+
+        //EXAMPLE: Override CalcBlock() using the unique property 
+        public override int CalcBlock()
+        {
+            return base.CalcBlock();
+
+            //Create an integer to store the calculated Block.
+            int calculatedBlock = Block;//Defaults to the regular Block.
+
+            //Create an instance of the Random object.
+            Random rand = new Random();
+
+            /*             
+             * Call the instance method Next() of the Random object.
+             * Store the results in an integer "percent"
+             * Pass 101 as a parameter into the Next() method
+             * Now we will never get values greater than 100.
+             * Our minimum value will be 1.
+             * Our maximum value will be 100.
+             * So effectively, there is a true 1/100 chance to get 
+             * any integer between 1 and 100 for the "percent"
+            */
+            int percent = rand.Next(1, 101);
+
+            //If the integer rolled for percent is less than or equal to the HidePercent chance
+            //(some number 1-100), then we'll grant the bonus for hiding in the shell.
+            if (percent <= HidePercent)
+            {
+                //So far the default Turtle, all values of 1-25 rolled for "percent" will trigger
+                //th additional Block, adding 20 bonus Block.
+                calculatedBlock += BonusBlock;
+            }
+
+            //Return the calculated Block! For the default turtle, if they didn't roll 1-25,
+            //they'll get the base Block of 15.
+            return calculatedBlock;
+            
+        }
+    }
+}
---
+++ Dungeon/DungeonLibrary/Vampire.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    //Inheritance:
+    public class Vampire : Monster
+    {
+        //Unique Props:
+        public bool IsShiny { get; set; }
+        public DateTime HourChangeBack { get; set; }
+
+        //Constructor with one unique prop:
+        public Vampire(string name, string description, int life, int maxLife, int hitChance, int block, 
+            int minDamage, int maxDamage, bool isShiny) 
+            : base(name, description, hitChance, block, life, maxLife, minDamage, maxDamage)
+        {
+            HourChangeBack = DateTime.Now;//Default value for unique prop.
+            IsShiny = isShiny;
+
+            //EXAMPLE: Using a unique property to affect the assignment of the inherited properties:
+            //At night, our vampire becomes significantly more dangerous.
+            if (HourChangeBack.Hour < 6 || HourChangeBack.Hour > 18)
+            {
+                HitChance += 10;
+                Block += 10;
+                MaxDamage += 5;
+                MinDamage += 5;
+            }
+        }
+
+        //Empty Constructor
+        public Vampire()//takes in no parameters! 
+        {
+            //But we still assign all the properties.
+            Name = "Edward";
+            Description = "The love of my life";
+            MaxLife = 70;
+            Life = 60;
+            HitChance = 90;
+            Block = 0;
+            MinDamage = 20;
+            MaxDamage = 30;
+            IsShiny = true;
+
+            //Make sure to assign the default:
+            HourChangeBack = DateTime.Now;//Calculated at the time of constructing the object.
+            //And test to see if we should apply the buff:
+            if (HourChangeBack.Hour < 6 || HourChangeBack.Hour > 18)
+            {
+                HitChance += 10;
+                Block += 10;
+                MaxDamage += 5;
+                MinDamage += 5;
+            }
+            //This will save us time when constructing our objects.
+        }
+
+        //EXAMPLE: Overriding the ToString() using unique props
+        public override string ToString()
+        {
+            //Create a string to store the results of our calculation.
+            string strengthString = "";
+
+            if (HourChangeBack.Hour < 6 || HourChangeBack.Hour > 18 )//If the hour is less than (6am) or greater
+                                                                //than 18  (6pm)...                  
+            {
+                //Store a string indicating the increase in stats on Lines 26-32.
+                strengthString = "The ominous glow of the moon strengthens this creature.";
+            }
+            else
+            {
+                //Otherwise, store a string indicating that bonus is not applied.
+                strengthString = "The bright rays of the sun are weakening this creature.";
+            }
+
+            //Create a string to store the results of our calculation.
+            string shinyString = "";
+
+            if (IsShiny)//If the vampire is Shiny...
+            {
+                //Add some flavor text.
+                shinyString = "\nThis vampire sparkles in the light. How mesmerising and beautiful!";
+            }
+
+            //If the vampire is not shiny, it's defaulting to an empty string.
+
+            //Concatenate our flavor text to the base ToString().
+            return base.ToString() + $"\n{strengthString}" + $"{shinyString}";
+        }
+    }
+}
